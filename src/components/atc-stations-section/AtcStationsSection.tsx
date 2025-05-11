32b9682f5bf86174d4bb605a60f161ddcce84849
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2025-05-11 13:57:07 -0400

    Add station buttons styling

--- src/components/atc-stations-section/AtcStationsSection.tsx
+++ src/components/atc-stations-section/AtcStationsSection.tsx
@@ -2,10 +2,43 @@ import VolumeSlider from '../components/VolumeSlider'
 import styles from './AtcStationsSection.module.scss'
 
 const AtcStationsSection = () => {
+  const stationsData = [
+    { name: 'BOS ', isActive: true },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+    { name: 'BOS ', isActive: false },
+  ]
+
   return (
     <div className={styles.section}>
-      <h2>ATC Stations</h2>
-      <VolumeSlider />
+      <div className={styles.header}>
+        <h2>ATC Stations</h2>
+        <VolumeSlider />
+      </div>
+      <div className={styles.stationButtonsContainer}>
+        {stationsData.map((station, i) => (
+          <button
+            key={`${station.name}-${i}`}
+            className={`${styles.stationButton} ${
+              station.isActive && styles.activeButton
+            }`}
+          >
+            {station.name}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
