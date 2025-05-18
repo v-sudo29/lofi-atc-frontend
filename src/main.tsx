274b31408bd59ed6fd82814b6d534294e297b933
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2025-05-18 12:29:16 -0400

    Set up post hog

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import LightDarkModeProvider from './contexts/lightDarkModeContext.tsx'
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.PRD ? import.meta.env.VITE_PUBLIC_POSTHOG_HOST : '',
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LightDarkModeProvider>
-      <App />
-    </LightDarkModeProvider>
+    <PostHogProvider
+      apiKey={
+        import.meta.env.PRD ? import.meta.env.VITE_PUBLIC_POSTHOG_KEY : ''
+      }
+      options={options}
+    >
+      <LightDarkModeProvider>
+        <App />
+      </LightDarkModeProvider>
+    </PostHogProvider>
   </StrictMode>
 )
