783f0f90f215d0c234034f01595ac6eed94caaf6
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2025-05-26 22:14:59 -0400

    Optimize rendering audio elements

--- src/components/MainWidget.tsx
+++ src/components/MainWidget.tsx
@@ -19,7 +19,7 @@ import lofiCoffeeMidnightAudio from '../assets/lofiAudio/coffee-at-midnight.mp3'
 import lofiCobblestoneAudio from '../assets/lofiAudio/cobblestone-reverie.mp3'
 import lofiMoonlightAudio from '../assets/lofiAudio/moonlight-in-the-keep.mp3'
 
-import atcBosAudio from '../assets/atcAudio/May-23-2025-BOS.mp3'
+import atcBosAudio from '../assets/atcAudio/May-26-2025-BOS.mp3'
 import atcSfoAudio from '../assets/atcAudio/May-23-2025-SFO.mp3'
 import atcLaxAudio from '../assets/atcAudio/May-23-2025-LAX.mp3'
 import atcPhxAudio from '../assets/atcAudio/May-23-2025-PHX.mp3'
@@ -28,40 +28,6 @@ import atcJfkAudio from '../assets/atcAudio/May-23-2025-JFK.mp3'
 import atcOrdAudio from '../assets/atcAudio/May-23-2025-ORD.mp3'
 import atcAtlAudio from '../assets/atcAudio/May-23-2025-ATL.mp3'
 import atcDenAudio from '../assets/atcAudio/May-23-2025-DEN.mp3'
-
-import BrownV1Audio from '../assets/color-noise-audio/brownV1.mp3'
-import BrownV2Audio from '../assets/color-noise-audio/brownV2.mp3'
-import BrownV3Audio from '../assets/color-noise-audio/brownV3.mp3'
-import GreyAudio from '../assets/color-noise-audio/grey.mp3'
-import PinkV1Audio from '../assets/color-noise-audio/pinkV1.mp3'
-import PinkV2Audio from '../assets/color-noise-audio/pinkV2.mp3'
-import WhiteV1Audio from '../assets/color-noise-audio/whiteV1.mp3'
-
-import CampfireFireAudio from '../assets/nature-audio/campfire.mp3'
-import CicadasAudio from '../assets/nature-audio/cicadas.mp3'
-import MarshAudio from '../assets/nature-audio/marsh.mp3'
-import MorningV1Audio from '../assets/nature-audio/morning v1.mp3'
-import MorningV2Audio from '../assets/nature-audio/morning v2.mp3'
-import NightV1Audio from '../assets/nature-audio/nightv1.mp3'
-import NightV2Audio from '../assets/nature-audio/nightv2.mp3'
-import RainV1Audio from '../assets/nature-audio/rainv1.mp3'
-import RainV2Audio from '../assets/nature-audio/rainv2.mp3'
-import StreamV1Audio from '../assets/nature-audio/streamv1.mp3'
-import StreamV2Audio from '../assets/nature-audio/streamv2.mp3'
-import ThunderV1Audio from '../assets/nature-audio/thunderv1.mp3'
-import ThunderV2Audio from '../assets/nature-audio/thunderv2.mp3'
-import WaterfallAudio from '../assets/nature-audio/waterfall.mp3'
-import WavesAudio from '../assets/nature-audio/waves.mp3'
-import WindAudio from '../assets/nature-audio/wind.mp3'
-
-import AcAudio from '../assets/urban-audio/ac.mp3'
-import ChatterV1Audio from '../assets/urban-audio/chatterv1.mp3'
-import ChatterV2Audio from '../assets/urban-audio/chatterv2.mp3'
-import FansAudio from '../assets/urban-audio/fans.mp3'
-import FryingAudio from '../assets/urban-audio/frying.mp3'
-import TrafficV1Audio from '../assets/urban-audio/trafficv1.mp3'
-import TrafficV2Audio from '../assets/urban-audio/trafficv2.mp3'
-import TrainsAudio from '../assets/urban-audio/trains.mp3'
 
 import ColorNoiseImg from '../assets/images/color-noise.png'
 import AmbienceOptionsImg from '../assets/images/atc-stations.png'
@@ -110,7 +76,7 @@ const lofiSongsData: AudioData[] = [
 export interface AmbienceCategoryData {
   name: string
   imgSrc: string
-  allAudio: AudioData[]
+  allAudio: AudioData[] | null
 }
 
 const ambienceCategoriesData: AmbienceCategoryData[] = [
@@ -132,111 +98,17 @@ const ambienceCategoriesData: AmbienceCategoryData[] = [
   {
     name: AMBIENCE_CATEGORIES.COLOR_NOISE,
     imgSrc: ColorNoiseImg,
-    allAudio: [
-      {
-        name: 'Brown V1',
-        audio: new Audio(BrownV1Audio),
-      },
-      {
-        name: 'Brown V2',
-        audio: new Audio(BrownV2Audio),
-      },
-      {
-        name: 'Brown V3',
-        audio: new Audio(BrownV3Audio),
-      },
-      {
-        name: 'Grey',
-        audio: new Audio(GreyAudio),
-      },
-      {
-        name: 'Pink V1',
-        audio: new Audio(PinkV1Audio),
-      },
-      {
-        name: 'Pink V2',
-        audio: new Audio(PinkV2Audio),
-      },
-      {
-        name: 'White V1',
-        audio: new Audio(WhiteV1Audio),
-      },
-    ],
+    allAudio: null, // starting value
   },
   {
     name: AMBIENCE_CATEGORIES.NATURE,
     imgSrc: NatureImg,
-    allAudio: [
-      {
-        name: 'Campfire',
-        audio: new Audio(CampfireFireAudio),
-      },
-      {
-        name: 'Cicadas',
-        audio: new Audio(CicadasAudio),
-      },
-      {
-        name: 'Marsh',
-        audio: new Audio(MarshAudio),
-      },
-      {
-        name: 'Morning V1',
-        audio: new Audio(MorningV1Audio),
-      },
-      {
-        name: 'Morning V2',
-        audio: new Audio(MorningV2Audio),
-      },
-      { name: 'Night V1', audio: new Audio(NightV1Audio) },
-      { name: 'Night V2', audio: new Audio(NightV2Audio) },
-      { name: 'Rain V1', audio: new Audio(RainV1Audio) },
-      { name: 'Rain V2', audio: new Audio(RainV2Audio) },
-      { name: 'Stream V1', audio: new Audio(StreamV1Audio) },
-      { name: 'Stream V2', audio: new Audio(StreamV2Audio) },
-      { name: 'Thunder V1', audio: new Audio(ThunderV1Audio) },
-      { name: 'Thunder V2', audio: new Audio(ThunderV2Audio) },
-      { name: 'Waterfall', audio: new Audio(WaterfallAudio) },
-      { name: 'Waves', audio: new Audio(WavesAudio) },
-      { name: 'Wind', audio: new Audio(WindAudio) },
-    ],
+    allAudio: null, // starting value
   },
   {
     name: AMBIENCE_CATEGORIES.URBAN,
     imgSrc: UrbanImg,
-    allAudio: [
-      {
-        name: 'AC',
-        audio: new Audio(AcAudio),
-      },
-      {
-        name: 'Chatter V1',
-        audio: new Audio(ChatterV1Audio),
-      },
-      {
-        name: 'Chatter V2',
-        audio: new Audio(ChatterV2Audio),
-      },
-      {
-        name: 'Fans',
-        audio: new Audio(FansAudio),
-      },
-      {
-        name: 'Frying',
-        audio: new Audio(FryingAudio),
-      },
-      {
-        name: 'Traffic V1',
-        audio: new Audio(TrafficV1Audio),
-      },
-      {
-        name: 'Traffic V2',
-        audio: new Audio(TrafficV2Audio),
-      },
-      {
-        name: 'Trains',
-        audio: new Audio(TrainsAudio),
-      },
-    ],
+    allAudio: null,
   },
 ]
 
@@ -247,11 +119,12 @@ const startingAmbience = ambienceCategoriesData.filter(
 export const MainWidget = () => {
   const [isAudioPlaying, setIsAudioPlaying] = useState(false)
   const [currentAmbienceCategory, setAmbienceCategory] =
-    useState(startingAmbience)
+    useState<AmbienceCategoryData>(startingAmbience)
   const [currentSong, setCurrentSong] = useState<AudioData>(lofiSongsData[0])
-  const [currentAmbience, setCurrentAmbience] = useState<AudioData>(
-    currentAmbienceCategory.allAudio[0]
-  )
+  const [currentAmbience, setCurrentAmbience] = useState<AudioData | null>(null)
+  const [allAmbienceCategoriesData, setAllAmbienceCategoriesData] = useState<
+    AmbienceCategoryData[]
+  >(ambienceCategoriesData)
   const { mode } = useLightDarkMode()
 
   const lofiDropdownRef = useRef<HTMLSelectElement>(null)
@@ -337,7 +210,7 @@ export const MainWidget = () => {
   }
 
   const handlePlayNextAmbience = () => {
-    if (!currentAmbienceCategory) return
+    if (!currentAmbienceCategory.allAudio) return
 
     let nextAtcIndex = 0
     const lastIndex = currentAmbienceCategory.allAudio.length - 1
@@ -372,6 +245,14 @@ export const MainWidget = () => {
     }
   }, [currentAmbience])
 
+  // Set current ambience if it doesn't exist
+  useEffect(() => {
+    if (!currentAmbience && currentAmbienceCategory.allAudio) {
+      setCurrentAmbience(currentAmbienceCategory.allAudio[0])
+    }
+  }, [currentAmbience])
+
+  if (!ambienceCategoriesData) return
   return (
     <div
       className={clsx(styles.mainWidget, {
@@ -390,10 +271,11 @@ export const MainWidget = () => {
       <AtcStationsSection
         handleAmbienceVolumeUpdate={handleAmbienceVolumeUpdate}
         handleAmbienceOptionClick={handleAmbienceOptionClick}
-        ambienceCategoriesData={ambienceCategoriesData}
         currentAmbience={currentAmbience}
         currentAmbienceCategory={currentAmbienceCategory}
         setAmbienceCategory={setAmbienceCategory}
+        allAmbienceCategoriesData={allAmbienceCategoriesData}
+        setAllAmbienceCategoriesData={setAllAmbienceCategoriesData}
       />
       <PlayButton
         handlePlayLofiAndAmbience={handlePlayLofiAndAmbience}
--- src/components/atc-stations-section/AtcStationsSection.tsx
+++ src/components/atc-stations-section/AtcStationsSection.tsx
@@ -4,25 +4,59 @@ import { VolumeSlider } from '../components/VolumeSlider'
 import { MODE } from '../../constants/LightDarkMode'
 import { AmbienceCategoryData, AudioData } from '../MainWidget'
 import { ChevronDownIcon } from '../../assets/ChevronDownIcon'
+import { FilledCheckIcon } from '../../assets/FilledCheckIcon'
+import { AMBIENCE_CATEGORIES } from '../../constants/AmbienceCategories'
+
+import BrownV1Audio from '../../assets/color-noise-audio/brownV1.mp3'
+import BrownV2Audio from '../../assets/color-noise-audio/brownV2.mp3'
+import BrownV3Audio from '../../assets/color-noise-audio/brownV3.mp3'
+import GreyAudio from '../../assets/color-noise-audio/grey.mp3'
+import PinkV1Audio from '../../assets/color-noise-audio/pinkV1.mp3'
+import PinkV2Audio from '../../assets/color-noise-audio/pinkV2.mp3'
+import WhiteV1Audio from '../../assets/color-noise-audio/whiteV1.mp3'
+
+import CampfireFireAudio from '../../assets/nature-audio/campfire.mp3'
+import CicadasAudio from '../../assets/nature-audio/cicadas.mp3'
+import MarshAudio from '../../assets/nature-audio/marsh.mp3'
+import MorningV1Audio from '../../assets/nature-audio/morning v1.mp3'
+import NightV2Audio from '../../assets/nature-audio/nightv2.mp3'
+import RainV1Audio from '../../assets/nature-audio/rainv1.mp3'
+import StreamV2Audio from '../../assets/nature-audio/streamv2.mp3'
+import WaterfallAudio from '../../assets/nature-audio/waterfall.mp3'
+import WavesAudio from '../../assets/nature-audio/waves.mp3'
+import WindAudio from '../../assets/nature-audio/wind.mp3'
+
+import AcAudio from '../../assets/urban-audio/ac.mp3'
+import ChatterV1Audio from '../../assets/urban-audio/chatterv1.mp3'
+import ChatterV2Audio from '../../assets/urban-audio/chatterv2.mp3'
+import FansAudio from '../../assets/urban-audio/fans.mp3'
+import FryingAudio from '../../assets/urban-audio/frying.mp3'
+import TrafficV1Audio from '../../assets/urban-audio/trafficv1.mp3'
+import TrafficV2Audio from '../../assets/urban-audio/trafficv2.mp3'
+import TrainsAudio from '../../assets/urban-audio/trains.mp3'
+
 import styles from './AtcStationsSection.module.scss'
 import clsx from 'clsx'
-import { FilledCheckIcon } from '../../assets/FilledCheckIcon'
 
 export const AtcStationsSection = ({
   handleAmbienceVolumeUpdate,
   handleAmbienceOptionClick,
-  ambienceCategoriesData,
   currentAmbience,
   currentAmbienceCategory,
   setAmbienceCategory,
+  allAmbienceCategoriesData,
+  setAllAmbienceCategoriesData,
 }: {
   handleAmbienceVolumeUpdate: (e: React.ChangeEvent<HTMLInputElement>) => void
   handleAmbienceOptionClick: (atcStation: AudioData) => void
-  ambienceCategoriesData: AmbienceCategoryData[]
   currentAmbience: AudioData | null
   currentAmbienceCategory: AmbienceCategoryData
   setAmbienceCategory: React.Dispatch<
     React.SetStateAction<AmbienceCategoryData>
+  >
+  allAmbienceCategoriesData: AmbienceCategoryData[]
+  setAllAmbienceCategoriesData: React.Dispatch<
+    React.SetStateAction<AmbienceCategoryData[]>
   >
 }) => {
   const { mode } = useLightDarkMode()
@@ -31,7 +65,155 @@ export const AtcStationsSection = ({
     setIsCategoriesVisible(!isCategoriesVisible)
   }
   const handleSwitchCategory = (categoryData: AmbienceCategoryData) => {
-    if (currentAmbienceCategory?.name !== categoryData.name) {
+    const categoryName = categoryData.name
+    const currentCategoryObj = allAmbienceCategoriesData.find(
+      (categoryObj) => categoryObj.name === categoryName
+    )
+    const isAudioFetched = currentCategoryObj?.allAudio
+
+    if (
+      currentAmbienceCategory?.name !== categoryData.name &&
+      !isAudioFetched
+    ) {
+      if (categoryData.name === AMBIENCE_CATEGORIES.COLOR_NOISE) {
+        // create new category obj with new Audio elements
+        const categoryDataWithAudio = {
+          ...categoryData,
+          allAudio: [
+            {
+              name: 'Brown V1',
+              audio: new Audio(BrownV1Audio),
+            },
+            {
+              name: 'Brown V2',
+              audio: new Audio(BrownV2Audio),
+            },
+            {
+              name: 'Brown V3',
+              audio: new Audio(BrownV3Audio),
+            },
+            {
+              name: 'Grey',
+              audio: new Audio(GreyAudio),
+            },
+            {
+              name: 'Pink V1',
+              audio: new Audio(PinkV1Audio),
+            },
+            {
+              name: 'Pink V2',
+              audio: new Audio(PinkV2Audio),
+            },
+            {
+              name: 'White V1',
+              audio: new Audio(WhiteV1Audio),
+            },
+          ],
+        }
+        setAllAmbienceCategoriesData((prev) => {
+          const newArr = prev.map((obj) => {
+            if (obj.name === categoryName) {
+              const newObj = { ...obj }
+              newObj.allAudio = categoryDataWithAudio.allAudio
+              return newObj
+            }
+            return obj
+          })
+          return newArr
+        })
+        setAmbienceCategory(categoryDataWithAudio)
+      } else if (categoryData.name === AMBIENCE_CATEGORIES.NATURE) {
+        const categoryDataWithAudio = {
+          ...categoryData,
+          allAudio: [
+            {
+              name: 'Campfire',
+              audio: new Audio(CampfireFireAudio),
+            },
+            {
+              name: 'Cicadas',
+              audio: new Audio(CicadasAudio),
+            },
+            {
+              name: 'Marsh',
+              audio: new Audio(MarshAudio),
+            },
+            {
+              name: 'Morning',
+              audio: new Audio(MorningV1Audio),
+            },
+            { name: 'Night', audio: new Audio(NightV2Audio) },
+            { name: 'Rain', audio: new Audio(RainV1Audio) },
+            { name: 'Stream', audio: new Audio(StreamV2Audio) },
+            { name: 'Waterfall', audio: new Audio(WaterfallAudio) },
+            { name: 'Waves', audio: new Audio(WavesAudio) },
+            { name: 'Wind', audio: new Audio(WindAudio) },
+          ],
+        }
+        setAllAmbienceCategoriesData((prev) => {
+          const newArr = prev.map((obj) => {
+            if (obj.name === categoryName) {
+              const newObj = { ...obj }
+              newObj.allAudio = categoryDataWithAudio.allAudio
+              return newObj
+            }
+            return obj
+          })
+          return newArr
+        })
+        setAmbienceCategory(categoryDataWithAudio)
+      } else if (categoryData.name === AMBIENCE_CATEGORIES.URBAN) {
+        const categoryDataWithAudio = {
+          ...categoryData,
+          allAudio: [
+            {
+              name: 'AC',
+              audio: new Audio(AcAudio),
+            },
+            {
+              name: 'Chatter V1',
+              audio: new Audio(ChatterV1Audio),
+            },
+            {
+              name: 'Chatter V2',
+              audio: new Audio(ChatterV2Audio),
+            },
+            {
+              name: 'Fans',
+              audio: new Audio(FansAudio),
+            },
+            {
+              name: 'Frying',
+              audio: new Audio(FryingAudio),
+            },
+            {
+              name: 'Traffic V1',
+              audio: new Audio(TrafficV1Audio),
+            },
+            {
+              name: 'Traffic V2',
+              audio: new Audio(TrafficV2Audio),
+            },
+            {
+              name: 'Trains',
+              audio: new Audio(TrainsAudio),
+            },
+          ],
+        }
+        setAllAmbienceCategoriesData((prev) => {
+          const newArr = prev.map((obj) => {
+            if (obj.name === categoryName) {
+              const newObj = { ...obj }
+              newObj.allAudio = categoryDataWithAudio.allAudio
+              return newObj
+            }
+            return obj
+          })
+          return newArr
+        })
+        setAmbienceCategory(categoryDataWithAudio)
+      }
+    } else {
       setAmbienceCategory(categoryData)
     }
   }
@@ -71,7 +253,7 @@ export const AtcStationsSection = ({
       </div>
       {isCategoriesVisible && (
         <div className={styles.ambienceCategoriesContainer}>
-          {ambienceCategoriesData.map((category, i) => {
+          {allAmbienceCategoriesData.map((category, i) => {
             return (
               <button
                 key={`${category.name}-${i}`}
